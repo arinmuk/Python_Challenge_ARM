374ab6b04a960aeffad3a4042dd6674cfd7aa94b
Arin <arin_muk@hotmail.com>
2018-11-24 13:18:35 -0500

    pybank 5 out 5 solved. workin on way to create list for the csv file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,15 @@
 import os
 import csv
+
+#Total Months: 86
+#Total: $38382578
+#Average  Change: $-2315.12
+#Greatest Increase in Profits: Feb-2012 ($1926159)
+#Greatest Decrease in Profits: Sep-2013 ($-2196167)
+
+
+
+
 def average(numbers):
     length = len(numbers)
     total = 0.0
@@ -13,6 +23,9 @@ lst_pl = []
 header =[]
 avg_delta=[]
 sorted_deltas_pl=[]
+final_header =["Total Months","Total","Average  Change","Greatest Increase in Profits","Greatest DEcrease in Profits"]
+final_value=[]
+dict_results={}
 grt_inc_month=""
 grt_dec_month=""
 grt_inc_pr_indx = 0
@@ -45,15 +58,25 @@ grt_dec_pr_indx = avg_delta.index(grt_dec_pr_val)
 grt_inc_month=lst_month[grt_inc_pr_indx+1]
 grt_dec_month=lst_month[grt_dec_pr_indx+1]
 
-print(" Highest profit for the month of " +grt_inc_month+ " = " + str(grt_inc_pr_val) )
-print(" Lowest profit for the month of " +grt_dec_month+ " = " + str(grt_dec_pr_val) )
 
 tot_mths = len(lst_month)
 grand_tot_pl=sum(lst_pl)
 avg_chng=average(avg_delta)
-print(header)
-print(lst_month)
-print(lst_pl)
-print("total Months :" + str(tot_mths))
-print("Total Profit/loss :" + str(grand_tot_pl))
-print("Average change between months : "+ str(round(avg_chng,2)))
+#print(header)
+#print(lst_month)
+#print(lst_pl)
+print("Financial Analysis")
+print("---------------------------------------------------")
+print("Total Months: " + str(tot_mths))
+print("Total: " + str('${:,.2f}'.format(grand_tot_pl,2)))
+print("Average Change: "+ str('${:,.2f}'.format(avg_chng,2)))
+print("Greatest Increase in Profits: " +grt_inc_month+ " (" + str('${:,.2f}'.format(grt_inc_pr_val,0))+")")
+print("Greatest Decrease in Profits: " +grt_dec_month+ " (" + str('${:,.2f}'.format(grt_dec_pr_val,0))+")" )
+final_value.append(tot_mths)
+final_value.append('${:,.2f}'.format(grand_tot_pl,2))
+final_value.append('${:,.2f}'.format(avg_chng,2))
+final_value.append('${:,.2f}'.format(grt_inc_pr_val,0))
+final_value.append('${:,.2f}'.format(grt_dec_pr_val,0))
+print("==================================================")
+print(final_header)
+print(final_value)
