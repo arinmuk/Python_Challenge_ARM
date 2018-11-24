6128442f6c493854f4e19c4bd0db3221eb6c7334
Arin <arin_muk@hotmail.com>
2018-11-24 12:32:36 -0500

    pybank 5 out 5 solved. have to figure out how to print the csv file

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,6 +12,13 @@ lst_month = []
 lst_pl = []
 header =[]
 avg_delta=[]
+sorted_deltas_pl=[]
+grt_inc_month=""
+grt_dec_month=""
+grt_inc_pr_indx = 0
+grt_dec_pr_indx = 0
+grt_inc_pr_val = 0.0
+grt_dec_pr_val = 0.0
 tot_mths =0
 grand_tot_pl = 0.0
 indx=0
@@ -29,10 +36,17 @@ for indx in range(1,len(lst_pl)) :
     #print(float(lst_pl[indx-1]))
     #print(avg_delta)
 
+sorted_deltas_pl=sorted(avg_delta,reverse=True)
+print(sorted_deltas_pl)
+grt_inc_pr_val = sorted_deltas_pl[0]
+grt_dec_pr_val = sorted_deltas_pl[-1]
+grt_inc_pr_indx = avg_delta.index(grt_inc_pr_val)
+grt_dec_pr_indx = avg_delta.index(grt_dec_pr_val)
+grt_inc_month=lst_month[grt_inc_pr_indx+1]
+grt_dec_month=lst_month[grt_dec_pr_indx+1]
 
-
-
-
+print(" Highest profit for the month of " +grt_inc_month+ " = " + str(grt_inc_pr_val) )
+print(" Lowest profit for the month of " +grt_dec_month+ " = " + str(grt_dec_pr_val) )
 
 tot_mths = len(lst_month)
 grand_tot_pl=sum(lst_pl)
